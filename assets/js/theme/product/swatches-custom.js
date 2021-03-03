c190534014d3ffb310f6f748c38b6c587fb69f6b
silifonov <silifonov@gmail.com>
2021-03-03 19:29:20 +0200

    comments added

--- assets/js/theme/product/swatches-custom.js
+++ assets/js/theme/product/swatches-custom.js
@@ -1,50 +1,51 @@
 export default function customSwatches() {
-  const dropdown = $('.custom-swatch-select');
+    const dropdown = $('.custom-swatch-select');
 
-  if (dropdown.length) {
-    $(dropdown).each(function () {
-      const select = $(this);
-      const dropdownSelectId = $(select).attr('id');
-      const modalSwatch = $(select).find('.swatch-modal');
-      const dropdownSelectText = $(select).find('.selected-swatch');
-      const dropdownSelectColor = $(select).find('.selected-color');
-      const selectedColorOnHover = $(modalSwatch).find('.swatch-selected');
+    if (dropdown.length) {
+        // Execute on each product variant options
+        $(dropdown).each(function () {
+            const select = $(this);
+            const container = document.getElementById($(select).attr('id'));
+            const modalSwatch = $(select).find('.swatch-modal');
+            const dropdownSelectText = $(select).find('.selected-swatch');
+            const dropdownSelectColor = $(select).find('.selected-color');
+            const selectedColorOnHover = $(modalSwatch).find('.swatch-selected');
 
-      // Show custom swatches on dropdown click
-      $(this).on('click touch', () => {
-        const elId = $(modalSwatch).attr('id');
-        if ($(modalSwatch).css('display') === 'none') {
-          $(modalSwatch).show(200); // show current swatch options
-          $(`.custom-swatch-select .swatch-modal:not(#${elId})`).hide(200); // hide others modal swatch options
-        } else {
-          $(modalSwatch).hide(200);
-        }
-      });
+            // Show custom modal with swatches on dropdown click
+            $(this).on('click touch', () => {
+                const elId = $(modalSwatch).attr('id');
+                if ($(modalSwatch).css('display') === 'none') {
+                    $(modalSwatch).show(200); // show current swatch options
+                    $(`.custom-swatch-select .swatch-modal:not(#${elId})`).hide(200); // hide others modal swatch options
+                } else {
+                    $(modalSwatch).hide(200);
+                }
+            });
 
-      // Swatch onHover & click
-      $(modalSwatch).find('.form-option-variant').each(function () {
-        $(this).hover(function () {
-          $(selectedColorOnHover).text(this.title);
+            // Bind events
+            $(modalSwatch).find('.form-option-variant').each(function () {
+                // Swatch hover
+                $(this).hover(function () {
+                    $(selectedColorOnHover).text(this.title);
+                });
+
+                // Swatch click
+                $(this).closest('.form-option-swatch').on('touchstart click', function(){
+                    const clone = $(this).children('.form-option-variant').clone();
+                    $(dropdownSelectText).text($(clone).attr('title'));
+                    $(dropdownSelectColor).html(clone);
+                    $(select).css('padding-left', '45px');
+                });
+            });
+
+            // Click outside custom dropdown
+            if ($(container).length) {
+                document.addEventListener('click', (e) => {
+                    if (container !== e.target && !container.contains(e.target)) {
+                        $(modalSwatch).hide(200);
+                    }
+                });
+            }
         });
-
-        // Swatch click
-        $(this).closest('.form-option-swatch').on('touchstart click', function(){
-          const clone = $(this).children('.form-option-variant').clone();
-          $(dropdownSelectText).text($(clone).attr('title'));
-          $(dropdownSelectColor).html(clone);
-          $(select).css('padding-left', '45px');
-        });
-      });
-
-      // Click outside custom dropdown
-      const container = document.getElementById(dropdownSelectId);
-      if ($(container).length) {
-        document.addEventListener('click', function (event) {
-          if (container !== event.target && !container.contains(event.target)) {
-            $(modalSwatch).hide(200);
-          }
-        });
-      }
-    });
-  }
+    }
 }
